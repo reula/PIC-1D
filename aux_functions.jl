c5faeb2c4d5d9e136edc898a6a351b3acc5bb952
Oscar Reula <oreula@gmail.com>
2023-02-28 12:52:08 -0300

    changing bounds on all the density and current functions.

--- aux_functions.jl
+++ aux_functions.jl
@@ -184,10 +184,11 @@ function get_density!(u, n, par_grid, shift)
   r = view(u,1:N)
   fill!(n,0.0)
   # Evaluate number density.
+  bound = Int64(ceil(order/2))
   for i in 1:N
     @inbounds j, y = get_index_and_y(r[i],J,L)
     y += - shift
-    for l in (-order):order 
+    for l in (-bound):(bound+1) 
       @inbounds n[mod1(j + l, J)] += Shape(order, -y + l) / dx / n0; # the dx here is from the different definition from the paper
     end
   end
@@ -208,14 +209,15 @@ function get_density_2D!(u, n, par_grid, shift)
   y = [0.0,0.0]
   #@show u
   # Evaluate number density.
+  bound = Int64(ceil(order/2))
   for i in 1:N
     s = (i-1)*2D + 1
     u_r = view(u,s:(s+D-1))
     #@inbounds j, y = get_index_and_y!(j,y,u_r,J,Box)
     @inbounds get_index_and_y!(j,y,u_r,J,Box)
     y .= y .- shift # shift must be the same in all directions!
-    for l in (-order):order 
-      for m in (-order):order
+    for l in (-bound):(bound+1) 
+      for m in (-bound):(bound+1)
       #@inbounds n[mod1(j + l, J)] += Shape(order, -y + l) / dx / n0; # the dx here is from the different definition from the paper
       @inbounds n[mod1(j[1] + l, J[1]), mod1(j[2] + m, J[2])] += Shape(order, -y[1] + l) * Shape(order, -y[2] + m)/ n0
       end
@@ -241,14 +243,15 @@ function get_density_threads_2D!(u, n, par, shift)
   #s = [0 for i in 1:nthreads()]
   n0 = N
   # Evaluate number density.
+  bound = Int64(ceil(order/2))
   @threads  for i in 1:N
               #s[threadid()] = (i-1)*2D + 1
               #u_r[:,threadid()] = view(u,s[threadid()]:(s[threadid()]+D-1))
               #j[:,threadid()], y[:,threadid()] = get_index_and_y!(j[:,threadid()], y[:,threadid()], u_r[:,threadid()],J , Box) 
               j[:,threadid()], y[:,threadid()] = get_index_and_y!(j[:,threadid()], y[:,threadid()], u[(i-1)*2D + 1:(i-1)*2D + D],J , Box) 
       @inbounds  y[:,threadid()] .= y[:,threadid()] .- shift # shift must be the same in all directions!
-              for l in (-order):order 
-                for m in (-order):order
+              for l in (-bound):(bound+1)
+                for m in (-bound):(bound+1)
       @inbounds   Tn[mod1(j[1,threadid()] + l, J[1]), mod1(j[2,threadid()] + m, J[2]), threadid()] += Shape(order, -y[1,threadid()] + l) * Shape(order, -y[2,threadid()] + m)
                 end
               end
@@ -272,17 +275,18 @@ function get_density_threads!(u, n, p, shift)
   y = fill(Float64(1.0),nthreads())
   Tn .= zeros(Float64)
   n0 = N/L
+  bound = Int64(ceil(order/2))
   # Evaluate number density.
   @threads for i in 1:N
     @inbounds j[threadid()], y[threadid()] = get_index_and_y(u[i], J, L)
     y[threadid()] += - shift
-    for l in (-order):-j[threadid()]
+    for l in (-bound):-j[threadid()]
       @inbounds Tn[J + j[threadid()] + l, threadid()] += Shape(order, -y[threadid()] + l) 
     end
-    for l in max(-order,-j[threadid()]+1):min(order,J-j[threadid()])
+    for l in max(-bound,-j[threadid()]+1):min((bound+1),J-j[threadid()])
       @inbounds Tn[j[threadid()] + l, threadid()] += Shape(order, -y[threadid()] + l) 
     end
-    for l in J-j[threadid()]+1:order
+    for l in J-j[threadid()]+1:(bound+1)
       @inbounds Tn[j[threadid()] - J + l, threadid()] += Shape(order, -y[threadid()] + l) 
     end
   end
@@ -320,10 +324,11 @@ function get_current_rel!(u, S, par_grid)
   p = view(u,N+1:2N) # in the relativistic version we compute p instead of v
   fill!(S,0.0)
   n0 = N/L
+  bound = Int64(ceil(order/2))
   for i in 1:N
     @inbounds j, y = get_index_and_y(r[i],J,L)
     @inbounds v = p2v(p[i]) / dx / n0 # the dx here is from the different definition from the paper
-    for l in (-order):order 
+    for l in (-bound):(bound+1) 
       @inbounds S[mod1(j + l, J)] += Shape(order, -y + l) * v;
     end
   end
@@ -337,16 +342,17 @@ function get_current_rel_threads!(u, S, p)
   y = fill(Float64(1.0),nthreads())
   TS .= zeros(Float64)
   n0 = N/L
+  bound = Int64(ceil(order/2))
   @threads for i in 1:N
     @inbounds j[threadid()], y[threadid()] = get_index_and_y(u[i], J, L)
     @inbounds v = p2v(u[N+i]) / dx / n0 # the dx here is from the different definition from the paper
-    for l in (-order):-j[threadid()]
+    for l in (-bound):-j[threadid()]
       @inbounds TS[J + j[threadid()] + l, threadid()] += Shape(order, -y[threadid()] + l) * v
     end
-    for l in max(-order,-j[threadid()]+1):min(order,J-j[threadid()])
+    for l in max(-bound,-j[threadid()]+1):min(bound+1,J-j[threadid()])
       @inbounds TS[j[threadid()] + l, threadid()] += Shape(order, -y[threadid()] + l) * v
     end
-    for l in J-j[threadid()]+1:order
+    for l in J-j[threadid()]+1:(bound+1)
       @inbounds TS[j[threadid()] - J + l, threadid()] += Shape(order, -y[threadid()] + l) * v
     end
     #= 
@@ -371,6 +377,7 @@ function get_current_rel_2D!(u, S, par_grid;shift=0.0)
     error("dimension mismach")
    end
   #vol = volume(Box)
+  bound = Int64(ceil(order/2))
   fill!(S,[0.0,0.0])
   v = Array{Float64}(undef,2)
   #n0 = N/vol # correct expression but not needed
@@ -385,8 +392,8 @@ function get_current_rel_2D!(u, S, par_grid;shift=0.0)
     @inbounds  y[:,threadid()] .= y[:,threadid()] .- shift # shift must be the same in all directions!
     #@inbounds v = p2v(p) / vol / n0 # correct but can be made simpler
     @inbounds v = p2v(p) / n0 # dividimos aquí para hacerlo más eficiente.
-    for l in (-order):order 
-      for m in (-order):order
+    for l in (-bound):(bound+1) 
+      for m in (-bound):(bound+1)
       @inbounds S[mod1(j[1] + l, J[1]), mod1(j[2] + m, J[2])] += Shape(order, -y[1] + l) * Shape(order, -y[2] + m) * v;
       end
     end
@@ -404,7 +411,9 @@ function get_current_threads_2D!(u, S, par; shift=0.0)
   D = 2::Int64
   if D != length(J) 
     error("dimension mismach")
-   end
+  end
+  bound = Int64(ceil(order/2))
+
   #u_r = Array{Float64}(undef,(D,nthreads()))
   j = Array{Int64}(undef,2,nthreads())
   j .= 1
@@ -425,8 +434,8 @@ function get_current_threads_2D!(u, S, par; shift=0.0)
               #j[:,threadid()], y[:,threadid()] = get_index_and_y!(j[:,threadid()], y[:,threadid()], u_r[:,threadid()],J , Box) 
        j[:,threadid()], y[:,threadid()] = get_index_and_y!(j[:,threadid()], y[:,threadid()], u[(i-1)*2D + 1:(i-1)*2D + D],J , Box) 
        y[:,threadid()] .= y[:,threadid()] .- shift # shift must be the same in all directions!
-              for l in (-order):order 
-                for m in (-order):order
+              for l in (-bound):(bound+1)
+                for m in (-bound):(bound+1)
        TS[:,mod1(j[1,threadid()] + l, J[1]), mod1(j[2,threadid()] + m, J[2]), threadid()] += Shape(order, -y[1,threadid()] + l) * Shape(order, -y[2,threadid()] + m)*v[:,threadid()]
                 end
               end
