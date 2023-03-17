289f155d51f818d453babe7e323708e7db89785d
Carlos Bederian <carlos.bederian@unc.edu.ar>
2023-03-17 18:19:14 -0300

    Value types version of Shape + W

--- aux_functions/aux_functions_shapes.jl
+++ aux_functions/aux_functions_shapes.jl
@@ -65,3 +65,53 @@ While the orders of W goes from 0 to 5
 @inline function Shape(order::Int, y::Float64)
   return W(order - 1, y)
 end
+
+
+@inline function W(::Val{0}, y::T)::T where {T<:AbstractFloat}
+  y = abs(y)
+  (y <= 0.5) ? 1.0 : 0.0
+end
+
+@inline function W(::Val{1}, y::T)::T where {T<:AbstractFloat}
+  y = abs(y)
+  (y <= 1.0) ? 1.0 - y : 0.0
+end
+
+@inline function W(::Val{2}, y::T)::T where {T<:AbstractFloat}
+  y = abs(y)
+  #return (y <= 1/2) ? 3/4 - y^2  : (((y > 1/2) && (y <= 3/2)) ? (3 - 2*y)^2 / 8 : 0)
+  (y <= 0.5) ? 0.75 - y^2 : (((y <= 1.5)) ? (3.0 - 2.0 * y)^2 / 8.0 : 0.0)
+end
+
+@inline function W(::Val{3}, y::T)::T where {T<:AbstractFloat}
+  y = abs(y)
+  #return (y <= 1) ? 2/3 - y^2 + y^3 / 2 : (((y > 1) && (y <= 2)) ? (2 - y)^3 / 6 : 0)
+  (y <= 1.0) ? 2.0 / 3.0 - y^2 + y^3 / 2.0 : ((y <= 2) ? (2 - y)^3 / 6.0 : 0.0)
+end
+
+@inline function W(::Val{4}, y::T)::T where {T<:AbstractFloat}
+  y = abs(y)
+  #return (y <= 1/2) ? 115/192 - 5y^2/8 + y^4/4 : (((y > 1/2) && (y <= 3/2)) ? (55 + 20y -120y^2 + 80y^3 - 16y^4)/96 : (((y > 3/2) && (y < 5/2)) ? (5 - 2y)^4/384 : 0))
+  (y <= 0.5) ? 115 / 192 - 5y^2 / 8 + y^4 / 4 : (((y <= 3 / 2)) ? (55 + 20y - 120y^2 + 80y^3 - 16y^4) / 96 : (((y < 5 / 2)) ? (5 - 2y)^4 / 384 : 0.0))
+end
+
+@inline function W(::Val{5}, y::T)::T where {T<:AbstractFloat}
+  y = abs(y)
+  #return (y <= 1) ? 11/20 - y^2/2 + y^4/4 - y^5/12 : (((y > 1) && (y <= 2)) ? 17/40 + 5y/8 - 7y^2/4 + 5y^3/4 - 3y^4/8 + y^5/24 : (((y > 2) && (y < 3)) ? (3 - y)^5/120 : 0))
+  (y <= 1) ? 11 / 20 - y^2 / 2 + y^4 / 4 - y^5 / 12 : (((y <= 2)) ? 17 / 40 + 5y / 8 - 7y^2 / 4 + 5y^3 / 4 - 3y^4 / 8 + y^5 / 24 : (((y < 3)) ? (3 - y)^5 / 120 : 0.0))
+end
+
+"""
+Shape functions are W functions of an order less.
+The dx in the Appendix is added when used so as not to carry dx all over.
+So this definition DIFFERS FROM THE PAPER BY A DX!
+They have support on -order/2 =< y =< order/2
+The orders goes from 1 to 6 (order zero is not defined)
+While the orders of W goes from 0 to 5
+"""
+@inline Shape(::Val{1}, y::AbstractFloat) = W(Val(0), y)
+@inline Shape(::Val{2}, y::AbstractFloat) = W(Val(1), y)
+@inline Shape(::Val{3}, y::AbstractFloat) = W(Val(2), y)
+@inline Shape(::Val{4}, y::AbstractFloat) = W(Val(3), y)
+@inline Shape(::Val{5}, y::AbstractFloat) = W(Val(4), y)
+@inline Shape(::Val{6}, y::AbstractFloat) = W(Val(5), y)
