24fd240e555fdfbe22d9f5f57e905517412a87fe
Carlos Bederian <carlos.bederian@unc.edu.ar>
2023-04-05 04:11:39 -0300

    Fix get_current_threads_2D params order

--- aux_functions/aux_functions_density_charge.jl
+++ aux_functions/aux_functions_density_charge.jl
@@ -289,7 +289,7 @@ The output is an array of type (2,J1,J2). Checked and working OK against the oth
 function get_current_threads_2D!(u::Array{Float64,1}, S::Array{Float64,3}, par; shift=0.0) #WITH DIFFERENT LAYOUT
   #par_grid, Tn, j, y = par # no vale la pena en cuanto a tiempo ni memoria
   par_grid, TS = par
-  N, J, Box, order = par_grid
+  N, Box, J, order = par_grid
   D = 2::Int64
   bound = Int64(ceil(order/2))
   if D != length(J) 
