cc94f360a86001e84a9c8e798851e564a5e8c94c
Carlos Bederian <carlos.bederian@unc.edu.ar>
2023-05-19 04:40:18 -0300

    Marcar RHS_D_slim! como funcion que modifica los parametros

--- Tests/test_speed_RHS.jl
+++ Tests/test_speed_RHS.jl
@@ -208,12 +208,12 @@ val_order = Val(order)
 
 RHS_D(du_ref,u,p_RHS_D); # here we save the output before changes
 #RHS_D_opt(du,u,p_RHS_D);
-RHS_D_slim(val_order,du,u,p_RHS_D);
+RHS_D_slim!(val_order,du,u,p_RHS_D);
 #@profile RHS_D_slim(du,u,p_RHS_D);
 #VSCodeServer.@profview RHS_D_slim(du,u,p_RHS_D);
 
 @show norm(du_ref - du)
-@btime RHS_D_slim($val_order,$du,$u,$p_RHS_D);
+@btime RHS_D_slim!($val_order,$du,$u,$p_RHS_D);
 
 
 # N = 10^5
